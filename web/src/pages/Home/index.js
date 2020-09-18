28f5797f1ec23e148c77ce9d28f0a32c364bd738
pedroraimondi <pedroksk8@hotmail.com>
2020-09-18 19:36:16 -0300

    add home page

---
+++ web/src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PokeBola from '../../components/PokeBola';
+
+// import './App.css';
+
+function Home() {
+    return (
+      <>
+        <PokeBola>
+          <h1>Parabéns! <br />Você encontrou o manual pokemon</h1>
+  
+        </PokeBola>
+      </>
+    );
+  }
+  
+  export default Home;
+  
