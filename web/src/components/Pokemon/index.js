cd4aba706573f9183cfa5ae591bf6b1f2e065d15
pedroraimondi <pedroksk8@hotmail.com>
2020-09-19 04:45:56 -0300

    add pokemon infos

--- web/src/components/Pokemon/index.js
+++ web/src/components/Pokemon/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import capitalizeFistLetter from '../../util/capitalizeFistLetter';
 
 import './styles.css'
 
-function Pokemon({name, url}) {
+function Pokemon({name, url, pokemon}) {
     const pokemonIndex = url.split("/")[url.split("/").length -2];
     const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true;`
 
     return (
-        <div className="pokeInfo">
-            <h1>#{pokemonIndex}</h1>
-            <h1>{name}</h1><br/>
-            <img src={imageUrl}/>
+        <div className="pokemon">
+            <img src={imageUrl} />
+            <div className="pokemonInfo">
+                <h3>#{pokemonIndex}</h3>
+                <h2>{capitalizeFistLetter(name)}</h2>
+            </div>
         </div>
     );
 }
